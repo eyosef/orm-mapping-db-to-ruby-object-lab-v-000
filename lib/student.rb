7e82e12c94ed2f3b5bb0706165258549d223d16d
Eden Mesfun <emesfun@gmail.com>
2018-03-14 14:43:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    binding.pry
+
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
