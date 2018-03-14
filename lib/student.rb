67ea0ee35529801546493e6f7dfea0c0012546da
Eden Mesfun <emesfun@gmail.com>
2018-03-14 14:43:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,9 @@ class Student
   def self.new_from_db(row)
     binding.pry
     new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.name = row[2]
     # create a new Student object given a row from the database
   end
 
